57550acabf658bec17e48fddf3d798efaab9fb4a
David Mauricio Gomez Zapata <davidmaogomezz@gmail.com>
2021-06-21 10:05:28 -0500

    Add firestore

---
+++ src/db/firestore.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const config = {
+  apiKey: "AIzaSyBom6Vm1uY3xiVrSazRf_AIxG5LawD_qOo",
+  authDomain: "instructor-online.firebaseapp.com",
+  projectId: "instructor-online",
+  storageBucket: "instructor-online.appspot.com",
+  messagingSenderId: "473220569591",
+  appId: "1:473220569591:web:d5cd9e3832b39b9b7a76f2",
+  measurementId: "G-6060B59VHS"
+};
+
+const fireb = firebase.initializeApp(config)
+const store = fireb.firestore()
+
+export default store
