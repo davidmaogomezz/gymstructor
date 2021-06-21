d6e603c60adb6241fcb5d126a050d6992bd37353
David Mauricio Gomez Zapata <davidmaogomezz@gmail.com>
2021-06-21 11:50:42 -0500

    add header bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,39 +7,34 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import { Navbar, Nav } from 'react-bootstrap';
 
 function Header() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/sign-in">SignIn</Link>
-            </li>
-            <li>
-              <Link to="/sign-up">SignUp</Link>
-            </li>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-          </ul>
-        </nav>
+      <Navbar bg="light" expand="lg">
+        <Link className="navbar-brand" to="/">Gymstructor</Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Link className="nav-link" to="/sign-in">SignIn</Link>
+            <Link className="nav-link" to="/sign-up">SignUp</Link>   
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/sign-in">
-            <SignIn />
-          </Route>
-          <Route path="/sign-up">
-            <SignUp />
-          </Route>
-          <Route path="/home">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/sign-in">
+          <SignIn />
+        </Route>
+        <Route path="/sign-up">
+          <SignUp />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+
     </Router>
   );
 }
