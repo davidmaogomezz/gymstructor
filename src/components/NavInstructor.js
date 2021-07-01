f3da30a1375c6c37ab12dccf800f56ba7a8caf5b
David Mauricio Gomez Zapata <davidmaogomezz@gmail.com>
2021-06-30 19:06:00 -0500

    Check if auth in nav

--- src/components/NavInstructor.js
+++ src/components/NavInstructor.js
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import { Navbar } from 'react-bootstrap';
-import {useDispatch} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import {logoutAction} from '../redux/auth'
 
 const NavInstructor = () => {
+
+  const user = useSelector(store => store.auth.user)
 
   const dispatch = useDispatch()
 
@@ -16,7 +18,13 @@ const NavInstructor = () => {
     <Navbar bg="light" expand="lg">
       <Link className="navbar-brand" to="/">Gymstructor</Link>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <button className="nav-link" onClick={logout}>Salir</button>
+      {
+        user ? (
+          <button className="nav-link" onClick={logout}>Salir</button>
+        ) : (
+          <button className="nav-link" onClick={() => window.location.href = '/sign-in'}>Ingresar</button>
+        )
+      }
     </Navbar>    
   )
 }
